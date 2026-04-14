6f1a52710ea285f15ddcd5f308aca6902b123f7e
STUDENT\2412017 <sho042804@icloud.com>
2026-04-14 10:01:20 +0900

    ステージ選択を計3ステージ分作った

---
+++ OTTTO/Assets/Script/Start/stage 2.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; //‚±‚ê•K{‚Å‚·
+
+
+public class stage2 : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+
+}
---
+++ OTTTO/Assets/Script/Start/stage 3.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; //‚±‚ê•K{‚Å‚·
+
+
+public class stage3 : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+}
