aa07928b00d9f2bc368ec280e24f1a8793249c24
STUDENT\2412020 <ryu_ru12599955@icloud.com>
2026-05-20 16:30:44 +0900

    Idleモデルを導入した

--- OTTTO/Assets/Script/Camera/MouseLook.cs
+++ OTTTO/Assets/Script/Camera/MouseLook.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-public class MouseLook : MonoBehaviour
-{
-    public float sensitivity = 0.2f;
-
-    private void Update()
+public class MouseLook : MonoBehaviour 
+{ 
+    public float sensitivity = 0.2f; private void Update() 
     {
         Vector2 mouseDelta = Mouse.current.delta.ReadValue();
-
         transform.Rotate(0, mouseDelta.x * sensitivity, 0);
     }
 }
