93bfaa865336b4f0777af3b3645a11aa00cb8067
STUDENT\2412017 <sho042804@icloud.com>
2026-09-18 16:30:37 +0900

    ステージ１を作ったが暗証番号がバグったのですぐに修正します

--- OTTTO/Assets/Script/Password/ReturnPlayer.cs
+++ OTTTO/Assets/Script/Password/ReturnPlayer.cs
@@ -9,6 +9,7 @@ public class ReturnPlayer : MonoBehaviour
     {
         if(PlayerPrefs.GetInt("DoorUnlocked", 0) == 1)
         {
+            Vector3 safePos = returnPoint.position + Vector3.up * 0.5f;
             player.position = returnPoint.position;
             player.rotation = returnPoint.rotation;
         }
